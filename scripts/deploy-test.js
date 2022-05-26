fdb350fcc21de03041a2c6f0e48a05f2e7e0c36a
tuturu-tech <elvis.skozdopolj@gmail.com>
2022-05-26 02:20:09 +0200

    basic tests

--- scripts/deploy-test.js
+++ scripts/deploy-test.js
@@ -1,34 +1,11 @@
 const hre = require("hardhat");
 
 async function main() {
-	const Loot = await hre.ethers.getContractFactory("Loot");
-	const loot = await Loot.deploy("testURI");
+	const DRIP = await hre.ethers.getContractFactory("DRIP");
+	const drip = await DRIP.deploy(2, [10, 20]);
 
-	await loot.deployed();
-	console.log("Loot deployed to:", loot.address);
-
-	const Raider = await hre.ethers.getContractFactory("Raider");
-	const raider = await Raider.deploy(loot.address);
-
-	await raider.deployed();
-	console.log("Raider deployed to:", raider.address);
-
-	const Dungeon = await hre.ethers.getContractFactory("DungeonRaid");
-	const dungeon = await Dungeon.deploy(
-		raider.address,
-		loot.address,
-		4659,
-		[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-		[
-			[1, 2],
-			[3, 4],
-			[5, 6],
-			[7, 8],
-		]
-	);
-
-	await dungeon.deployed();
-	console.log("DungeonRaid deployed to:", dungeon.address);
+	await drip.deployed();
+	console.log("Drip deployed to:", drip.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- test/local-tests.js
+++ test/local-tests.js
@@ -13,167 +13,87 @@ var time = centerTime();
 
 const getBalance = ethers.provider.getBalance;
 
+async function mineNBlocks(n) {
+	for (let index = 0; index < n; index++) {
+		await ethers.provider.send("evm_mine");
+	}
+}
+
 describe("ERC1155Drip", function () {
-	let erc1155, owner, addr1, loot, raider, dungeon;
+	let erc1155, owner, addr1, drip1, drip2;
 	beforeEach(async function () {
 		[owner, addr1] = await ethers.getSigners();
+		drip1 = BN(10);
+		drip2 = BN(20);
 
-		/* const ERC1155 = await hre.ethers.getContractFactory("DRIP");
-		erc1155 = await ERC1155.deploy(2, [10, 20]);
-		await erc1155.deployed(); */
-
-		const Loot = await hre.ethers.getContractFactory("Loot");
-		loot = await Loot.deploy([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-
-		await loot.deployed();
-		console.log("Loot deployed to:", loot.address);
-
-		const Raider = await hre.ethers.getContractFactory("Raider");
-		raider = await Raider.deploy(loot.address, 10, 5010);
-
-		await raider.deployed();
-		console.log("Raider deployed to:", raider.address);
-
-		const Dungeon = await hre.ethers.getContractFactory("DungeonRaid");
-		dungeon = await Dungeon.deploy(
-			raider.address,
-			loot.address,
-			4659,
-			[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-			[
-				[1, 2],
-				[3, 4],
-				[5, 6],
-				[7, 8],
-			]
-		);
-		await dungeon.deployed();
+		const ERC1155 = await hre.ethers.getContractFactory("DRIP");
+		erc1155 = await ERC1155.deploy(2, [drip1, drip2]);
+		await erc1155.deployed();
 	});
 
-	describe("Minting", async function () {
-		it("Should mint", async function () {
-			await expect(
-				raider.mint(0, "tesla", { value: ethers.utils.parseEther("0.02") })
-			).to.not.be.reverted;
-		});
+	it("Should mint correct amount", async function () {
+		await expect(erc1155.mint(owner.address, 0, 10)).to.not.be.reverted;
+		expect(await erc1155.balanceOf(owner.address, 0)).to.equal(10);
+	});
+
+	it("Should burn correct amount", async function () {
+		await expect(erc1155.mint(owner.address, 0, 10)).to.not.be.reverted;
+		expect(await erc1155.balanceOf(owner.address, 0)).to.equal(10);
+		await expect(erc1155.burn(owner.address, 0, 10)).to.not.be.reverted;
+		expect(await erc1155.balanceOf(owner.address, 0)).to.equal(0);
+	});
+
+	it("Should drip correctly", async function () {
+		await expect(erc1155.startDripping(owner.address, 0, 1)).to.not.be.reverted;
+		await mineNBlocks(10);
+		expect(await erc1155.balanceOf(owner.address, 0)).to.equal(drip1.mul(10));
+	});
+
+	it("Should drip correctly with multiplier", async function () {
+		await expect(erc1155.startDripping(owner.address, 0, 2)).to.not.be.reverted;
+		await mineNBlocks(10);
+		expect(await erc1155.balanceOf(owner.address, 0)).to.equal(drip1.mul(20));
+	});
+
+	it("Should drip correctly to multiple people", async function () {
+		await expect(erc1155.startDripping(owner.address, 0, 2)).to.not.be.reverted;
+		await expect(erc1155.startDripping(addr1.address, 0, 1)).to.not.be.reverted;
+		await mineNBlocks(10);
+		expect(await erc1155.balanceOf(owner.address, 0)).to.equal(drip1.mul(22));
+		expect(await erc1155.balanceOf(addr1.address, 0)).to.equal(drip1.mul(10));
+	});
+
+	it("Should stop drip correctly", async function () {
+		await expect(erc1155.startDripping(owner.address, 0, 1)).to.not.be.reverted;
+		await mineNBlocks(9);
+		await expect(erc1155.stopDripping(owner.address, 0, 1)).to.not.be.reverted;
+		expect(await erc1155.balanceOf(owner.address, 0)).to.equal(drip1.mul(10));
+
+		await mineNBlocks(10);
+		expect(await erc1155.balanceOf(owner.address, 0)).to.equal(drip1.mul(10));
+	});
+
+	it("Should correctly transfer after mint", async function () {
+		await expect(erc1155.mint(owner.address, 0, 100)).to.not.be.reverted;
+		expect(await erc1155.balanceOf(owner.address, 0)).to.equal(100);
+		await expect(
+			erc1155.safeTransferFrom(owner.address, addr1.address, 0, 50, "0x")
+		).to.not.be.reverted;
+
+		expect(await erc1155.balanceOf(owner.address, 0)).to.equal(50);
+		expect(await erc1155.balanceOf(addr1.address, 0)).to.equal(50);
+	});
+
+	it("Should transfer correctly while dripping", async function () {
+		await expect(erc1155.startDripping(owner.address, 0, 1)).to.not.be.reverted;
+		await mineNBlocks(9);
+
+		await expect(
+			erc1155.safeTransferFrom(owner.address, addr1.address, 0, 50, "0x")
+		).to.not.be.reverted;
+		await expect(erc1155.stopDripping(owner.address, 0, 1)).to.not.be.reverted;
+
+		expect(await erc1155.balanceOf(owner.address, 0)).to.equal(60);
+		expect(await erc1155.balanceOf(addr1.address, 0)).to.equal(50);
 	});
 });
-
-/* describe("All contracts", function () {
-	let loot, raider, dungeon, owner, addr1, addr2;
-	beforeEach(async function () {
-		[owner, addr1, addr2] = await ethers.getSigners();
-
-		const Loot = await hre.ethers.getContractFactory("Loot");
-		loot = await Loot.deploy("testURI");
-		await loot.deployed();
-
-		const Raider = await hre.ethers.getContractFactory("Raider");
-		raider = await Raider.deploy(loot.address);
-		await raider.deployed();
-
-		const Dungeon = await hre.ethers.getContractFactory("DungeonRaid");
-		dungeon = await Dungeon.deploy(raider.address, loot.address, 1234);
-		await dungeon.deployed();
-	});
-
-	describe("Loot", async function () {
-		it("Should correctly mint items", async function () {
-			await expect(loot.mintItemFor(owner.address, 2)).to.not.be.reverted;
-			expect(await loot.totalSupply()).to.equal(1);
-			expect(await loot.balanceOf(owner.address, 2)).to.equal(1);
-
-			await expect(loot.mintItemFor(owner.address, 3)).to.not.be.reverted;
-			await expect(loot.mintItemFor(owner.address, 4)).to.not.be.reverted;
-			expect(await loot.totalSupply()).to.equal(3);
-			expect(await loot.balanceOf(owner.address, 3)).to.equal(1);
-			expect(await loot.balanceOf(owner.address, 4)).to.equal(1);
-		});
-	});
-
-	describe("Raider", async function () {
-		it("Should correctly mint and equip items", async function () {
-			await expect(
-				raider.mintWithItems({ value: ethers.utils.parseEther("0.02") })
-			).to.not.be.reverted;
-			expect(await loot.balanceOf(owner.address, 2)).to.equal(1);
-			expect(await loot.balanceOf(owner.address, 3)).to.equal(1);
-			expect(await loot.balanceOf(owner.address, 4)).to.equal(1);
-			expect(await raider.totalSupply()).to.equal(1);
-			expect(await raider.ownerOf(1)).to.equal(owner.address);
-			console.log(await raider._tokenDataOf(1));
-		});
-	});
-
-	describe("DungeonRaid", async function () {
-		beforeEach(async function () {
-			await expect(loot.setDungeonContract(dungeon.address)).to.not.be.reverted;
-			await expect(loot.setRaiderContract(raider.address)).to.not.be.reverted;
-
-			await expect(raider.setLootContract(loot.address)).to.not.be.reverted;
-			await expect(
-				raider.setDungeonContract(dungeon.address)
-			).to.not.be.reverted;
-
-			await expect(
-				raider.mintWithItems({ value: ethers.utils.parseEther("0.02") })
-			).to.not.be.reverted;
-			await expect(
-				raider
-					.connect(addr1)
-					.mintWithItems({ value: ethers.utils.parseEther("0.02") })
-			).to.not.be.reverted;
-		});
-
-		describe("mockGenerateDungeon", async function () {
-			it("Should not revert and print dungeon", async function () {
-				await expect(dungeon.mockGenerateDungeon()).to.not.be.reverted;
-				//console.log(await dungeon.dungeons(1));
-			});
-		});
-
-		
-
-		// Minting multiple items returns an increased power for the next item minted
-		describe("proposeDungeon", async function () {
-			it("Should correctly propose a dungeon and print it", async function () {
-				await expect(dungeon.mockGenerateDungeon()).to.not.be.reverted;
-				await expect(dungeon.connect(addr1).proposeDungeon(1, 2)).to.not.be
-					.reverted;
-				console.log(await dungeon.dungeons(1));
-				console.log(await raider._tokenDataOf(1));
-				console.log(await raider._tokenDataOf(2));
-				console.log(await raider.equippedItems(1, 0));
-			});
-
-			it("Should correctly join dungeon and print", async function () {
-				await expect(dungeon.mockGenerateDungeon()).to.not.be.reverted;
-				await expect(dungeon.connect(addr1).proposeDungeon(1, 2)).to.not.be
-					.reverted;
-				console.log(await dungeon.dungeons(1));
-				await expect(dungeon.joinDungeon(1, 1)).to.not.be.reverted;
-				console.log(await raider._tokenDataOf(1));
-				console.log(await raider._tokenDataOf(2));
-				console.log(await dungeon.dungeons(1));
-			});
-
-			it("Should correctly execute dungeon", async function () {
-				await expect(dungeon.mockGenerateDungeon()).to.not.be.reverted;
-				await expect(dungeon.connect(addr1).proposeDungeon(1, 2)).to.not.be
-					.reverted;
-				await expect(dungeon.joinDungeon(1, 1)).to.not.be.reverted;
-				await advanceTime(time.delta6m);
-				await expect(dungeon.mockExecuteDungeon(1)).to.not.be.reverted;
-				expect(await loot.balanceOf(addr1.address, 0)).to.equal(0);
-				expect(await loot.balanceOf(addr1.address, 1)).to.equal(0);
-				await expect(dungeon.claimReward(1, 1)).to.not.be.reverted;
-				expect(await loot.balanceOf(owner.address, 0)).to.equal(
-					ethers.utils.parseEther("500")
-				);
-				expect(await loot.balanceOf(owner.address, 1)).to.equal(
-					ethers.utils.parseEther("50")
-				);
-			});
-		});
-	});
-}); */
